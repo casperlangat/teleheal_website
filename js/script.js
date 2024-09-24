988d5af6dfc3ff04765e53c14d86c6c082508a33
langatcasper <langatcasper@gmail.com>
2024-09-24 23:28:15 +0300

    teleheal_website update

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ let menubar = document.querySelector('#menubar');
 let mynav = document.querySelector('.navbar');
 
 
-menubar.onclick = () =>{
+menubar.onclick = () => {
     menubar.classList.toggle('fa-times')
     mynav.classList.toggle('active')
 }
